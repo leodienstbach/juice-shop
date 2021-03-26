a7f140af61da697beb4b9155172d22ba7e3e00a1
leodienstbach <81297277+leodienstbach@users.noreply.github.com>
2021-03-26 12:18:57 +0100

    Update angular.js

--- routes/angular.js
+++ routes/angular.js
@@ -8,9 +8,11 @@ const utils = require('../lib/utils')
 
 module.exports = function serveAngularClient () {
   return ({ url }, res, next) => {
-    if (!utils.startsWith(url, '/api') && !utils.startsWith(url, '/rest')) {
+    if(utils.startsWith(url, '/dastauth')){
+       res.sendFile(path.resolve(__dirname, '../GitLab-DAST-Site-Validation-8852d5eb-83e8-413c-8d47-45c36fc29ad3.txt')
+    }elseif (!utils.startsWith(url, '/api') && !utils.startsWith(url, '/rest')) {
       res.sendFile(path.resolve(__dirname, '../frontend/dist/frontend/index.html'))
-    } else {
+    }else {
       next(new Error('Unexpected path: ' + url))
     }
   }
